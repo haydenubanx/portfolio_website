25483e3a5563d5d48253d98db7dc815382ef648f
Hayden Eubanks <haydenubanx@yahoo.com>
2024-04-28 15:58:20 +0100

    Fixed synchronization of project

--- HTMLStructure/index.php
+++ HTMLStructure/index.php
@@ -12,107 +12,44 @@
 $cookie_value = null;
 
 
+function updateVisitCount()
+{
+    if (isset($_COOKIE['timesVisited'])) {
+        $_COOKIE['timesVisited'] += 1;
+    } else {
+        $visitNumber = isset($_SESSION['visitNumber']) ? $_SESSION['visitNumber'] : (isset($_COOKIE['timesVisited']) ? $_COOKIE['timesVisited'] : 0);
+        $visitNumber += 1;
+    }
+    $_SESSION['VisitNumber'] = isset($_COOKIE['timesVisited']) ? $_COOKIE['timesVisited'] : $visitNumber;
+    $_SESSION['firstTimeVisiting'] = $_COOKIE['timesVisited'] <= 1 ? true : false;
+}
 
-	//If the user or timeVisited cookies have not been set
-	if (!isset($_COOKIE['user']) || !isset($_COOKIE['timesVisited'])) {
+function setVisitCookies($cookieName, $cookieValue)
+{
+    if (isset($_SESSION['firstName']) && isset($_SESSION['lastName'])) {
+        $cookieValue = $_SESSION['firstName'] . " " . $_SESSION['lastName'];
+        setcookie($cookieName, $cookieValue, time() + (86400 * 30), "/");
+        setcookie('timesVisited', $_SESSION['VisitNumber'], time() + (86400 * 30), "/");
+        $_SESSION['firstTimeVisiting'] = false;
+    }
+}
 
-			//Name for first cookie and default value
-			$cookie_name = 'user';
-			$cookie_value = '';
+// Starts a session and include the dependency
+session_start();
+include_once 'includes/DbConnection/db.php';
 
-		//If the session variable for the visit number has been set
-		if(isset($_SESSION['visitNumber'])) {
-			$visitNumber = $_SESSION['visitNumber'];
-		}
+$cookie_value = null;
+$cookie_name = 'user';
 
-		//Else if the cookie variable has been set
-		else if (isset($_COOKIE['timesVisited'])) {
-			$visitNumber =  $_COOKIE['timesVisited'];
-		}
+if (!isset($_COOKIE['user']) || !isset($_COOKIE['timesVisited'])) {
+    updateVisitCount();
+    setVisitCookies($cookie_name, $cookie_value);
+} else {
+    updateVisitCount();
+    setVisitCookies('user', $_SESSION['firstName'] . "&nbsp" . $_SESSION['lastName']);
+}
 
-		//Otherwise set the temporary variable to zero because it is the first visit
-		else {
-			$visitNumber = 0;
-		}
-
-
-		//If the cookie for timeVisited is set
-		if (isset($_COOKIE['timesVisited'])) {
-
-			//Increment visits by one
-			$_COOKIE['timesVisited'] += 1;
-
-			//Set the session variable equal to the cookie
-			$_SESSION['VisitNumber'] = $_COOKIE['timesVisited'];
-
-			//Set the variable for first visit equal to false
-			$_SESSION['firstTimeVisiting'] = false;
-
-		}
-
-		//If the number of visits is less than or equal to the first visit
-		else if ($visitNumber <= 1) {
-
-			//Increment the temp variable by one
-			$visitNumber += 1;
-
-			//Set the session variable equal to the temp variable
-			$_SESSION['VisitNumber'] = $visitNumber;
-
-			//Set the first time visiting to true
-			$_SESSION['firstTimeVisiting'] = true;
-
-		}
-
-
-
-
-		//If the first name and last name are both set
-		if (isset($_SESSION['firstName']) && isset($_SESSION['lastName'])) {
-
-			//Set the value to be passed to the cookie to be both first and last name
-			$cookie_value = $_SESSION['firstName']." ".$_SESSION['lastName'];
-
-			//Set the name cookie
-			setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-
-			//Set the times visited cookie
-			setcookie('timesVisited', $_SESSION['VisitNumber'], time() + (86400 * 30), "/");
-
-			//Set the first visit variable to false for future passes
-			$_SESSION['firstTimeVisiting'] = false;
-
-		}
-
-	}
-
-	//Otherwise
-	else {
-
-		//Increment the number of visits in the cookie
-		$_COOKIE['timesVisited'] += 1;
-
-		//Set the session visit number equal to the cookie
-		$_SESSION['VisitNumber'] = $_COOKIE['timesVisited'];
-
-		//Set the first time visiting variable to false
-		$_SESSION['firstTimeVisiting'] = false;
-
-		//If the first name and last name session variables are set
-		if(isset($_SESSION['firstName']) && isset($_SESSION['lastName'])) {
-
-			//Set the cookie value to both the first and last names seperated by a space
-			$cookie_value = $_SESSION['firstName']."&nbsp".$_SESSION['lastName'];
-
-			//Set the cookie for names
-			setcookie('user', $cookie_value, time() + (86400 * 30), "/");
-
-			//Set the cookie for number of visits
-			setcookie('timesVisited', $_SESSION['VisitNumber'], time() + (86400 * 30), "/");
-		}
-
-
-	}
+// Other parts of the code...
 
 		//If both cookies are set
 		if(isset($_COOKIE['user']) && isset($_COOKIE['timesVisited'])) {
