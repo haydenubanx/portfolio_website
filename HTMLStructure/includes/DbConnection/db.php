2423b48990d2341f8e9278479964353ac3817cd8
Hayden Eubanks <haydenubanx@yahoo.com>
2024-04-28 17:58:19 +0100

    Set DB connection to new db hosted at FreeSQLDatabase

--- HTMLStructure/includes/DbConnection/db.php
+++ HTMLStructure/includes/DbConnection/db.php
@@ -1,16 +1,16 @@
 <?php
 
 //Sets the servername variable to the local host where the server is located
-$serverName = "localhost";
+$serverName = "sql8.freesqldatabase.com";
 
 //Sets the user to login as root
-$userName = "root";
+$userName = "sql8702603";
 
-//Sets the password to an empty string
-$password = "";
+//Sets the password
+$password = "YFBhAhJQA5";
 
 //Sets the name of the database to be connected to company
-$dbName = "company";
+$dbName = "sql8702603";
 
 //All of the above variables are passed as parameters to the connect function to create teh database connection
 $dbConnection = mysqli_connect($serverName, $userName, $password, $dbName);
