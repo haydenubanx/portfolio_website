a13ef6f07fedaaef2a6685def090c415f23be34c
Hyden Eubanks <72290782+haydenubanx@users.noreply.github.com>
2024-09-16 19:43:10 +0100

    Update db.php

--- HTMLStructure/includes/DbConnection/db.php
+++ HTMLStructure/includes/DbConnection/db.php
@@ -1,22 +1,5 @@
 <?php
 
-//Sets the servername variable to the local host where the server is located
-$serverName = "ax4.h.filess.io";
-
-//Sets the user to login as root
-$userName = "HaydenPortfolioWebsite_eagerdeep";
-
-//Sets the password
-$password = "fde0cf893715fd120f10c6e0f4bad7829849d58f";
-
-//Sets the name of the database to be connected to the company
-$dbName = "HaydenPortfolioWebsite_eagerdeep";
-
-$port="3307";
-
-//All of the above variables are passed as parameters to the connect function to create the database connection
-$dbConnection = mysqli_connect($serverName, $userName, $password, $dbName, $port);
-
-$_SESSION['dbConnection'] = $dbConnection;
+//DB Connection Details ignored from Git Repo for security reasons
 
 ?>
