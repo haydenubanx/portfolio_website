abb7e4726d469e6d6e906f7df823d322961de649
Hayden Eubanks <haydenubanx@gmail.com>
2024-09-19 22:33:33 +0100

    Rewrote DB connection masking

--- HTMLStructure/includes/DbConnection/db.php
+++ HTMLStructure/includes/DbConnection/db.php
@@ -2,20 +2,24 @@
 
 //Sets the servername variable to the local host where the server is located
 $serverName = "YXg0LmguZmlsZXNzLmlv";
+$decodedServerName = base64_decode($serverName);
 
 //Sets the user to login as root
 $userName = "SGF5ZGVuUG9ydGZvbGlvV2Vic2l0ZV9lYWdlcmRlZXA=";
+$decodedUserName = base64_decode($userName);
 
 //Sets the password
 $password = "ZmRlMGNmODkzNzE1ZmQxMjBmMTBjNmUwZjRiYWQ3ODI5ODQ5ZDU4Zg==";
+$decodedPass = base64_decode($password);
 
 //Sets the name of the database to be connected to the company
 $dbName = "SGF5ZGVuUG9ydGZvbGlvV2Vic2l0ZV9lYWdlcmRlZXA=";
+$decodedDB = base64_decode($dbName);
 
 $port="3307";
 
 //All of the above variables are passed as parameters to the connect function to create the database connection
-$dbConnection = mysqli_connect(base64_decode($serverName), base64_decode($userName), base64_decode($password), base64_decode($dbName), $port);
+$dbConnection = mysqli_connect($decodedServerName, $decodedUserName, $decodedPass, $decodedDB, $port);
 
 $_SESSION['dbConnection'] = $dbConnection;
 
