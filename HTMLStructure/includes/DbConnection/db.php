7544695be994cbcf705aba2b9239fd69c344093b
Hayden Eubanks <haydenubanx@gmail.com>
2024-09-19 23:00:05 +0100

    Reverted Database

--- HTMLStructure/includes/DbConnection/db.php
+++ HTMLStructure/includes/DbConnection/db.php
@@ -1,23 +1,19 @@
 <?php
 
-$serverName = "db5016368926.hosting-data.io";
+$serverName = "YXg0LmguZmlsZXNzLmlv";
 
-$userName = "dbu5334433=";
+$userName = "SGF5ZGVuUG9ydGZvbGlvV2Vic2l0ZV9lYWdlcmRlZXA=";
 
-$password = "ExampleDatabasePassword123!&";
+$password = "ZmRlMGNmODkzNzE1ZmQxMjBmMTBjNmUwZjRiYWQ3ODI5ODQ5ZDU4Zg==";
 
-$dbName = "dbs13307710";
+$dbName = "SGF5ZGVuUG9ydGZvbGlvV2Vic2l0ZV9lYWdlcmRlZXA=";
 
-$port="3306";
+$port="3307";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 //All of the above variables are passed as parameters to the connect function to create the database connection
-$dbConnection = mysqli_connect($serverName, $userName, $password, $dbName, $port);
-
-if ($dbConnection->connect_error) {
-    die('<p>Failed to connect to MySQL: '. $dbConnection->connect_error .'</p>');
-}
+$dbConnection = mysqli_connect(base64_decode($serverName), base64_decode($userName), base64_decode($password), base64_decode($dbName), $port);
 
 /* Set the desired charset after establishing a connection */
 $dbConnection->set_charset('utf8mb4');
