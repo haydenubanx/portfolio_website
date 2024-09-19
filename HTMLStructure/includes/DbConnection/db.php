4e9ab1e291a84258ab795bc4f7b633aa4090547f
Hayden Eubanks <haydenubanx@gmail.com>
2024-09-19 22:44:14 +0100

    Rolled Back DB changes

--- HTMLStructure/includes/DbConnection/db.php
+++ HTMLStructure/includes/DbConnection/db.php
@@ -1,22 +1,22 @@
 <?php
 
-//Sets the servername variable to the local host where the server is located
-$serverName = "ax4.h.filess.io";
+$serverName = "YXg0LmguZmlsZXNzLmlv";
 
-//Sets the user to login as root
-$userName = "HaydenPortfolioWebsite_eagerdeep";
+$userName = "SGF5ZGVuUG9ydGZvbGlvV2Vic2l0ZV9lYWdlcmRlZXA=";
 
-//Sets the password
-$password = "fde0cf893715fd120f10c6e0f4bad7829849d58f";
+$password = "ZmRlMGNmODkzNzE1ZmQxMjBmMTBjNmUwZjRiYWQ3ODI5ODQ5ZDU4Zg==";
 
-//Sets the name of the database to be connected to the company
-$dbName = "HaydenPortfolioWebsite_eagerdeep";
+$dbName = "SGF5ZGVuUG9ydGZvbGlvV2Vic2l0ZV9lYWdlcmRlZXA=";
 
 $port="3307";
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 //All of the above variables are passed as parameters to the connect function to create the database connection
-$dbConnection = mysqli_connect($serverName, $userName, $password, $dbName, $port);
+$dbConnection = mysqli_connect(base64_decode($serverName), base64_decode($userName), base64_decode($password), base64_decode($dbName), $port);
+
+/* Set the desired charset after establishing a connection */
+$dbConnection->set_charset('utf8mb4');
 
 $_SESSION['dbConnection'] = $dbConnection;
 
-?>
