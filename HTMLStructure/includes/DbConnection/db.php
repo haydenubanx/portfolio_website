570d298412b8700a12850fb93e28c132ac5d795b
Hayden Eubanks <haydenubanx@gmail.com>
2024-09-19 22:35:27 +0100

    Rolled Back DB changes

--- HTMLStructure/includes/DbConnection/db.php
+++ HTMLStructure/includes/DbConnection/db.php
@@ -1,25 +1,22 @@
 <?php
 
 //Sets the servername variable to the local host where the server is located
-$serverName = "YXg0LmguZmlsZXNzLmlv";
-$decodedServerName = base64_decode($serverName);
+$serverName = "ax4.h.filess.io";
 
 //Sets the user to login as root
-$userName = "SGF5ZGVuUG9ydGZvbGlvV2Vic2l0ZV9lYWdlcmRlZXA=";
-$decodedUserName = base64_decode($userName);
+$userName = "HaydenPortfolioWebsite_eagerdeep";
 
 //Sets the password
-$password = "ZmRlMGNmODkzNzE1ZmQxMjBmMTBjNmUwZjRiYWQ3ODI5ODQ5ZDU4Zg==";
-$decodedPass = base64_decode($password);
+$password = "fde0cf893715fd120f10c6e0f4bad7829849d58f";
 
 //Sets the name of the database to be connected to the company
-$dbName = "SGF5ZGVuUG9ydGZvbGlvV2Vic2l0ZV9lYWdlcmRlZXA=";
-$decodedDB = base64_decode($dbName);
+$dbName = "HaydenPortfolioWebsite_eagerdeep";
 
 $port="3307";
 
 //All of the above variables are passed as parameters to the connect function to create the database connection
-$dbConnection = mysqli_connect($decodedServerName, $decodedUserName, $decodedPass, $decodedDB, $port);
+$dbConnection = mysqli_connect($serverName, $userName, $password, $dbName, $port);
 
 $_SESSION['dbConnection'] = $dbConnection;
 
+?>
